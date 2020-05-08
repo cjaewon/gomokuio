0cb411ffcef428d93fa4d5477aeaf5e2600e5dc6
cjaewon <jwn4492@outlook.com>
2020-05-08 12:26:08 +0900

    Create Ranking

--- client/ts/index.ts
+++ client/ts/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import '../css/index.css';
 import event from './event';
 import { bind } from './lib/utill';
+import User from './entity/User';
 
 let ws = new WebSocket(`${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}`);
 
@@ -44,9 +45,22 @@ const getRanking = async() => {
   const url = `${window.location.protocol}//${location.host}/api/ranking`;
 
   const response = await axios.get(url);
-  const { ranking } = response.data;
+  const { ranking }: { ranking: User[]  } = response.data;
 
-  document.getElementById('#ranking')!.nodeValue = ranking;
+  document.getElementById('ranking')!.innerHTML = `
+    <table>
+      <tr>
+        <th>이름</th>
+        <th>스코어</th>
+      </tr>
+      ${ranking.map(user => `
+        <tr>
+          <td>${user.username}</td>
+          <td>${user.score}</td>
+        </tr>
+      `)}
+    </table>
+  `;
 };
 
 getRanking();

107785aaa8c4b19b7fd371f764ded96915fb62da
cjaewon <jwn4492@outlook.com>
2020-06-27 19:09:31 +0900

    Update Design

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -1,2 +1,10 @@
 import '../styles/global.scss';
+import '../styles/style.scss';
 
+document.getElementsByTagName('form')[0].addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const inputElement = <HTMLInputElement>document.getElementById('username');
+  const username = inputElement.value;
+
+});

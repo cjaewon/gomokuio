e2aa6ca977e9485645caa63aad862e0e87da6ecd
cjaewon <jwn4492@outlook.com>
2020-06-27 20:53:03 +0900

    Update Client

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -4,7 +4,16 @@ import '../styles/style.scss';
 document.getElementsByTagName('form')[0].addEventListener('submit', async e => {
   e.preventDefault();
 
-  const inputElement = <HTMLInputElement>document.getElementById('username');
-  const username = inputElement.value;
+  const input = <HTMLInputElement>document.getElementById('username');
+  const button = <HTMLButtonElement>document.getElementById('username-button');
+  const username = input.value;
 
+  button.disabled = true;
+
+  await new Promise(r => setTimeout(r, 300));
+
+  button.innerText = '유저를 매칭하는 중 입니다.';
 });
+
+
+document.body.classList.add('dark');

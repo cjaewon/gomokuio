6a87a8bf6dea5a527c835789cd5c43fe251cc672
cjaewon <jwn4492@outlook.com>
2020-06-27 11:42:02 +0900

    Delete all file for v2.0.0

--- server/dist/lib/utill.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bind = (name, data) => {
-    return JSON.stringify({
-        name,
-        data,
-    });
-};
--- server/src/lib/utill.ts
+++
@@ -1,6 +0,0 @@
-export const bind = (name: string, data: any) => {
-  return JSON.stringify({
-    name,
-    data,
-  });
-};

7f9327ae0cd6e27b91c722ce66261db569f1afc0
cjaewon <jwn4492@outlook.com>
2020-06-28 15:49:38 +0900

    Update app -> server in server/app.ts

--- server/app.ts
+++ server/app.ts
@@ -13,14 +13,12 @@ const server = http.createServer(app.callback());
 const wss = new ws.Server({ server });
 
 app.use(serve(path.resolve(__dirname, '../dist')));
-
-
 wss.on('connection', ws => {
   ws.on('message', data => message(ws, <string>data));
   ws.on('close', () => close(ws));
 });
 
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   log.info('Server listening on port 3000');
 });
